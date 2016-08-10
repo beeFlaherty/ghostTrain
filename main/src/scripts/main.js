e468e0a3d657142e86dee18b519a90c3aba3e79b
David Storey <david@lostincode.co.uk>
2016-08-10 16:06:38 +0100

    Adding beginning of monsters

--- main/src/scripts/main.js
+++ main/src/scripts/main.js
@@ -29,6 +29,24 @@
 		}
 	};
 
+	var RoomComponent = Vue.extend({
+		template: '#room_template',
+		props: ['index', 'room', 'system']
+	});
+	Vue.component('gt-room', RoomComponent);
+
+	var MonsterComponent = Vue.extend({
+		template: '#monster_template',
+		props: ['type', 'sound', 'animation']
+	});
+	Vue.component('gt-monster', MonsterComponent);
+
+	var LightComponent = Vue.extend({
+		template: '#light_template',
+		props: ['type']
+	});
+	Vue.component('gt-light', LightComponent);
+
 	var ride = new Vue({
 		el: '#ride_output',
 		template: '#ride_template',
