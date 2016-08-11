b73792944770b5f29c4d25a9c5e6c805004dc69f
David Storey <david@lostincode.co.uk>
2016-08-11 18:59:23 +0100

    Specifying configurables

--- main/src/scripts/systemData.js
+++ main/src/scripts/systemData.js
@@ -8,6 +8,124 @@
 		'room': {
 			'width': 5,
 			'height': 10
+		},
+
+		'configurables': {
+			'music': [
+				{
+					'id': '',
+					'name': 'Creepy Music Box',
+					'mp3': ''
+				},
+				{
+					'id': '',
+					'name': 'Ride of the Valkyries',
+					'mp3': ''
+				},
+				{
+					'id': '',
+					'name': 'Heavy Metal',
+					'mp3': ''
+				}
+			],
+			'themes': [
+				{
+					'id': '',
+					'name': 'Castle',
+					'textures': {
+						'floor': '',
+						'walls': '',
+						'roof': ''
+					}
+				},
+				{
+					'id': '',
+					'name': 'Forest',
+					'textures': {
+						'floor': '',
+						'walls': '',
+						'roof': ''
+					}
+				},
+				{
+					'id': '',
+					'name': 'Graveyard',
+					'textures': {
+						'floor': '',
+						'walls': '',
+						'roof': ''
+					}
+				}
+			],
+			'lighting': [
+				{
+					'id': '',
+					'name': 'Dark'
+				},
+				{
+					'id': '',
+					'name': 'Bright'
+				},
+				{
+					'id': '',
+					'name': 'Flickering'
+				},
+			],
+			'monsters': [
+				{
+					'id': '',
+					'name': 'Zombie',
+					'model': '',
+					'texture': ''
+				},
+				{
+					'id': '',
+					'name': 'Mummy',
+					'model': '',
+					'texture': ''
+				},
+				{
+					'id': '',
+					'name': 'Vampire',
+					'model': '',
+					'texture': ''
+				},
+			],
+			'sounds': [
+				{
+					'id': '',
+					'name': 'Moan',
+					'mp3': ''
+				},
+				{
+					'id': '',
+					'name': 'Scream',
+					'mp3': ''
+				},
+				{
+					'id': '',
+					'name': 'Cackle',
+					'mp3': ''
+				}
+			],
+			'animations': [
+				{
+					'id': '',
+					'name': 'Pop-up'
+				},
+				{
+					'id': '',
+					'name': 'Drop-down'
+				},
+				{
+					'id': '',
+					'name': 'Slide-in'
+				},
+				{
+					'id': '',
+					'name': 'Slide-right'
+				}
+			]
 		}
 	};
 }());
