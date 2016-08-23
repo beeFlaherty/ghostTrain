b28cf1605daf06870fb2aaf0b36443656215e9cd
joerealise <joe.mcdowall@realise.com>
2016-08-23 13:47:01 +0100

    initial progress on kart movement

--- main/src/scripts/ride.js
+++ main/src/scripts/ride.js
@@ -1,92 +1,106 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	module.exports = function(userData, systemData, configurableData) {
+  module.exports = function(userData, systemData, configurableData) {
 
-	AFRAME.registerComponent('collider', {
-		schema: {},
-
-		init: function() {
-			this.el.sceneEl.addBehavior(this);
-		},
-
-		tick: function() {
-			var scene = this.el.sceneEl.object3D;
-			scene.updateMatrixWorld();
-
-			function getGlobalPosition(obj) {
-				var vector = new THREE.Vector3();
-				vector.setFromMatrixPosition( obj.matrixWorld );
-
-				return vector;
+    AFRAME.registerComponent('kart', {
+      schema: {},
+      init: function() {
+				this.element = this.el.object3D;
+				this.element.position.set(17, 4, 10);
+				this.target = new THREE.Vector3(17,4,-5);
+				this.time = 0;
+			},
+      tick: function(time) {
+				this.time += 0.0001;
+				this.element.position.lerp(this.target, this.time);
 			}
+    });
 
-			function distanceVector(v1, v2) {
-				var dx = v1.x - v2.x;
-				var dy = v1.y - v2.y;
-				var dz = v1.z - v2.z;
+    AFRAME.registerComponent('collider', {
+      schema: {},
 
-				return Math.sqrt( dx * dx + dy * dy + dz * dz );
+      init: function() {
+        this.el.sceneEl.addBehavior(this);
+      },
 
-				// .distanceTo( v ) ?
-			}
+      tick: function() {
+        var scene = this.el.sceneEl.object3D;
+        scene.updateMatrixWorld();
 
-			var thisPosition = getGlobalPosition(this.el.object3D);
-			var others = document.querySelectorAll('a-obj-model[collidable]');
+        function getGlobalPosition(obj) {
+          var vector = new THREE.Vector3();
+          vector.setFromMatrixPosition(obj.matrixWorld);
 
-			others.forEach(function(other) {
-				var otherPosition = getGlobalPosition(other.object3D);
-				var distance = distanceVector(thisPosition, otherPosition);
+          return vector;
+        }
 
-				if (distance < systemData.monster.triggerDistance && !other.is('hit')) {
-					other.addState('hit');
-					other.emit('hit');
-					other.setAttribute('visible', 'true');
-				}
-			});
-		}
-	});
+        function distanceVector(v1, v2) {
+          var dx = v1.x - v2.x;
+          var dy = v1.y - v2.y;
+          var dz = v1.z - v2.z;
 
-		var RoomComponent = Vue.extend({
-			template: '#room_template',
-			props: ['index', 'room']
-		});
-		Vue.component('gt-room', RoomComponent);
+          return Math.sqrt(dx * dx + dy * dy + dz * dz);
 
-		var ScareComponent = Vue.extend({
-			template: '#scare_template',
-			props: ['index', 'monster', 'sound', 'animation', 'pos'],
-			methods: {
-				getPosition: function(index) {
-					return  (this.$root.system.room.height * 0.5) - // Offset from center of room
-						(
-							(this.$root.system.room.height / (this.$root.system.monster.perroom + 1)) * (index + 1)
-						);
-				}
-			}
-		});
-		Vue.component('gt-scare', ScareComponent);
+          // .distanceTo( v ) ?
+        }
 
-		var AnimationComponent = Vue.extend({
-			template: '#animation_template',
-			props: ['type']
-		});
-		Vue.component('gt-animation', AnimationComponent);
+        var thisPosition = getGlobalPosition(this.el.object3D);
+        var others = document.querySelectorAll('a-obj-model[collidable]');
 
-		var LightComponent = Vue.extend({
-			template: '#light_template',
-			props: ['type']
-		});
-		Vue.component('gt-light', LightComponent);
+        others.forEach(function(other) {
+          var otherPosition = getGlobalPosition(other.object3D);
+          var distance = distanceVector(thisPosition, otherPosition);
 
-		var ride = new Vue({
-			el: '#ride_output',
-			template: '#ride_template',
-			data: {
-				user: userData,
-				system: systemData,
-				configurable: configurableData
-			}
-		});
-	};
+          if (distance < systemData.monster.triggerDistance && !other.is('hit')) {
+            other.addState('hit');
+            other.emit('hit');
+            other.setAttribute('visible', 'true');
+          }
+        });
+      }
+    });
+
+    var RoomComponent = Vue.extend({
+      template: '#room_template',
+      props: ['index', 'room']
+    });
+    Vue.component('gt-room', RoomComponent);
+
+    var ScareComponent = Vue.extend({
+      template: '#scare_template',
+      props: ['index', 'monster', 'sound', 'animation', 'pos'],
+      methods: {
+        getPosition: function(index) {
+          return (this.$root.system.room.height * 0.5) - // Offset from center of room
+            (
+              (this.$root.system.room.height / (this.$root.system.monster.perroom + 1)) * (index + 1)
+            );
+        }
+      }
+    });
+    Vue.component('gt-scare', ScareComponent);
+
+    var AnimationComponent = Vue.extend({
+      template: '#animation_template',
+      props: ['type']
+    });
+    Vue.component('gt-animation', AnimationComponent);
+
+    var LightComponent = Vue.extend({
+      template: '#light_template',
+      props: ['type']
+    });
+    Vue.component('gt-light', LightComponent);
+
+    var ride = new Vue({
+      el: '#ride_output',
+      template: '#ride_template',
+      data: {
+        user: userData,
+        system: systemData,
+        configurable: configurableData
+      }
+    });
+  };
 }());
