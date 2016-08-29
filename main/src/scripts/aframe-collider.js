0b90aa52e01bcd64e3c2e9f12333004a70fa0258
David Storey <david@lostincode.co.uk>
2016-08-29 15:34:06 +0100

    Moved Aframe kart and collier code into seperate files

---
+++ main/src/scripts/aframe-collider.js
@@ -0,0 +1,50 @@
+(function() {
+  'use strict';
+
+  module.exports = function(triggerDistance) {
+    function distanceVector(v1, v2) {
+      var dx = v1.x - v2.x;
+      var dy = v1.y - v2.y;
+      var dz = v1.z - v2.z;
+
+      return Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+      // .distanceTo( v ) ?
+    }
+
+    function getGlobalPosition(obj) {
+      var vector = new THREE.Vector3();
+      vector.setFromMatrixPosition(obj.matrixWorld);
+
+      return vector;
+    }
+
+    AFRAME.registerComponent('collider', {
+      schema: {},
+
+      init: function() {
+        this.el.sceneEl.addBehavior(this);
+        this.others = document.querySelectorAll('a-obj-model[collidable]');
+      },
+
+      tick: function() {
+        var scene = this.el.sceneEl.object3D;
+        scene.updateMatrixWorld();
+
+        var thisPosition = getGlobalPosition(this.el.object3D);
+
+        this.others.forEach(function(other) {
+          var otherPosition = getGlobalPosition(other.object3D);
+          var distance = distanceVector(thisPosition, otherPosition);
+
+          if (distance < triggerDistance &&
+            !other.is('hit')) {
+            other.addState('hit');
+            other.emit('hit');
+            other.setAttribute('visible', 'true');
+          }
+        });
+      }
+    });
+  };
+}());
