fb61d353960d34121e44502297c51f4388489d0d
Bee Flaherty <beeflaherty@gmail.com>
2016-09-08 23:01:17 +0100

    Basic styles for form page.
    I couldn’t think of a cool name ok….

--- main/src/scripts/ui.js
+++ main/src/scripts/ui.js
@@ -2,15 +2,18 @@
   'use strict';
 
   module.exports = function(systemData, userData) {
-console.log('AAAA', userData);
+
     var userDataContainer = new Vue({
       el: '#settings',
       data: userData,
       methods: {
         activate: function(event) {
           userData.active = true;
-          console.log('BBBB', userData);
           event.preventDefault();
+        },
+        next: function(event){
+          event.preventDefault();
+          console.log(this);
         }
       },
       ready: function() {
