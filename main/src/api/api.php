8cfbb1f9b7e53ac801a70e9a503c2ffb207fd1a7
David Storey <david@lostincode.co.uk>
2016-10-05 21:08:49 +0100

    Initial version of API for storing rides in DB

---
+++ main/src/api/api.php
@@ -0,0 +1,77 @@
+<?php
+
+require './common.php';
+
+init_call();
+
+// If the database hasn't been setup then do so now
+function create_structure() {
+	run_query('
+		CREATE TABLE IF NOT EXISTS `rides` (
+		  `key` varchar(30) NOT NULL,
+		  `data` varchar(3000) NOT NULL,
+		  `created` date NOT NULL,
+		  `ip` varchar(255) NOT NULL
+		);
+	');
+}
+
+
+function get_ride($key) {
+	$key = str_replace('\\', '', $key);
+
+	$query = run_query(sprintf('
+		SELECT `data` FROM `rides`
+		WHERE `key` = \'%s\';
+	', $key))->fetch(PDO::FETCH_ASSOC);
+
+	if (!$query || !$query['data']) {
+		send_message('error', 'No ride found for key');
+	} else {
+		send_message('success', $query['data']);
+	}
+
+	return;
+}
+
+function set_ride($data) {
+	$data = str_replace('\\', '', $data);
+
+	$query = run_query(sprintf('
+		SELECT `key` FROM `rides`
+		WHERE `data` = \'%s\';
+	', $data))->fetch(PDO::FETCH_ASSOC);
+
+	if (!$query || !$query['key']) {
+		$key = substr(hash('md5', 'abcdef' . microtime()), 0, 8);
+		$ip = $_SERVER['REMOTE_ADDR'];
+
+		$query = run_query(sprintf('
+			INSERT INTO `rides`
+			(`key`, `data`, `created`, `ip`)
+			VALUES(\'%s\', \'%s\', NOW(), \'%s\');
+		', $key, $data, $ip));
+
+		$query = run_query(sprintf('
+			SELECT `key` FROM `rides`
+			WHERE `data` = \'%s\';
+		', $data))->fetch(PDO::FETCH_ASSOC);
+
+		if (!$query) {
+			send_message('error', 'Failed writing to database');
+		} else {
+			send_message('success', $key);
+		}
+	} else {
+		send_message('success', $query['key']);
+	}
+
+	return;
+
+	// $result = run_query(sprintf('
+	// 	SELECT correct_answer FROM quickfire_questions WHERE id = %s;',
+	// 	$answer['id']
+	// ))->fetch(PDO::FETCH_ASSOC);
+
+	// return ($result['correct_answer'] == $answer['answer']);
+}
---
+++ main/src/api/common.php
@@ -0,0 +1,146 @@
+<?php
+
+require './config.php';
+
+date_default_timezone_set('GMT');
+
+// initiates the request handling - should be called at the start of all requests
+function init_call() {
+	// domain_check();
+	session_check();
+
+	$db = connect_database(); // Connect... to the... database...
+	create_structure(); // Ensure the data tables exist
+	process_action(); // Work how to respond
+
+	send_message('error', array('reason' => 'none', 'message' => 'Something went wrong'));
+}
+
+// What it says on the tin
+function connect_database() {
+	global $config, $db;
+
+	$db = new PDO(
+		'mysql:host=' . $config['db_host'] . ';dbname=' . $config['db_name'],
+		$config['db_user'],
+		$config['db_pass']
+	);
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, 1);
+
+	return $db;
+}
+
+// Run a SQL query
+function run_query($query) {
+	global $db;
+
+	try {
+		$statement = $db->prepare($query);
+		$statement->execute();
+
+		return $statement;
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
+
+	// $db = ORM::get_db();
+	// return $db->exec($query);
+}
+
+// Run a SQL query and return the number of lines returned
+function count_query($query) {
+	$statement = run_query($query);
+	return count($statement->fetchAll(PDO::FETCH_OBJ));
+}
+
+// Check if at least one row returns from a SQL query
+function exists_query($query) {
+	return (count_query($query) > 0);
+}
+
+// Generated a unique ID
+function make_id($base) {
+	return substr(hash('md5', $base . microtime()), 0, 6);
+}
+
+// Check for and tidy up POST variables
+function get_post($name) {
+	if (!isset($_POST[$name])) {
+		return '';
+	} else {
+		return clean_variable($_POST[$name]);
+	}
+}
+
+// Check for the presence of a session variable (contents don't actually matter)
+function session_check() {
+	if (!isset($_POST['session'])) {
+		send_message('error', array('reason' => 'no_session_token','message' => 'No session token passed'));
+	}
+}
+
+// Make it slightly harder for cross-domain access to the API
+function domain_check() {
+	if (!isset($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], $_SERVER['SERVER_NAME']) == FALSE) {
+		send_message('error', array('reason' => 'wrong_domain','message' => 'Cross-domain calls are not allowed'));
+	}
+}
+
+// Work out which API action to perform
+function process_action() {
+	if (!isset($_POST['action'])) send_message('error', array('reason' => 'no_action', 'message' => 'No action specified'));
+
+	$action = clean_variable($_POST['action']);
+
+	switch($action) {
+		case 'get_ride':
+			$params = array(get_post('key'));
+			break;
+		case 'set_ride':
+			$params = array(get_post('data'));
+			break;
+
+		default:
+			send_message('error', array('reason' => 'unknown_action', 'message' => 'Unrecognised action specified'));
+			return;
+	}
+
+	call_user_func_array($action, $params);
+}
+
+
+// We've done all we can, pass feedback to the user now
+function send_message($status, $message) {
+	$GLOBALS['json_response']['status'] = $status;
+
+	if(is_array($message)) {
+		$GLOBALS['json_response'] = array_merge($GLOBALS['json_response'], $message);
+	} else {
+		$GLOBALS['json_response']['message'] = $message;
+	}
+
+	respond($GLOBALS['json_response']);
+}
+
+// Send a JSON response to the user
+function respond($arr) {
+	header('Content-Type: application/json');
+	header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+	header('Cache-Control: post-check=0, pre-check=0', false);
+	header('Pragma: no-cache');
+
+	exit(json_encode($arr));
+}
+
+// Make sure there's nothing nasty in a user input
+function clean_variable($variable) {
+	return mysql_escape_mimic(trim($variable));
+}
+
+function mysql_escape_mimic($inp) {
+	if (!empty($inp) && is_string($inp)) {
+		return str_replace(array('\\', "\0", "\n", "\r", "'", '"', "\x1a"), array('\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z'), $inp);
+	}
+
+	return $inp;
+}
