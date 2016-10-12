d895c8426e2d5d4751997107d72de58178feda83
David Storey <david@lostincode.co.uk>
2016-10-12 13:25:23 +0100

    Doors can now be faked/non-animated

--- main/src/scripts/components/door.js
+++ main/src/scripts/components/door.js
@@ -4,7 +4,7 @@
   module.exports = function(Vue) {
     var DoorComponent = Vue.extend({
       template: '#door_template',
-      props: ['position', 'rotation', 'theme']
+      props: ['position', 'rotation', 'theme', 'triggerable']
     });
     Vue.component('gt-door', DoorComponent);
   };
