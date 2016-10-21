2534da7eb8e548374b7771a73330e2034a7466b3
MacDoland <joe.mcdowall@gmail.com>
2016-10-21 21:42:41 +0100

    Fixed kart rotation bug

--- main/src/scripts/aframe/kart.js
+++ main/src/scripts/aframe/kart.js
@@ -2,8 +2,39 @@
   'use strict';
 
   module.exports = function(AFRAME) {
-    function lerp(a, b, f) {
-      return (a * (1 - f)) + (b * f);
+    function lerp(start, end, duration) {
+      return (start * (1 - duration)) + (end * duration);
+    }
+
+     function lerpDegrees(start, end, amount)
+    {
+        var difference = Math.abs(end - start);
+        if (difference > 180)
+        {
+            // We need to add on to one of the values.
+            if (end > start)
+            {
+                // We'll add it on to start...
+                start += 360;
+            }
+            else
+            {
+                // Add it on to end.
+                end += 360;
+            }
+        }
+
+        // Interpolate it.
+        var value = (start + ((end - start) * amount));
+
+        // Wrap it..
+        var rangeZero = 360;
+
+        if (value >= 0 && value <= 360){
+            return value;
+          }
+
+        return (value % rangeZero);
     }
 
     function compareOrder(a, b) {
@@ -93,7 +124,6 @@
         if (value > 360) {
            value = 360 - value;
         }
-        console.log(value);
         value = Math.radians(value);
 
         this.rotation = new THREE.Vector3(0, value, 0);
@@ -117,11 +147,14 @@
           this.percentComplete = this.timeSinceStart / this.duration;
 
           //    this.element.quaternion.slerp(this.rotation, 1);
-          value = lerp(
-            this.startRotation.y, this.rotation.y,
+          value = lerpDegrees(
+            Math.degrees(this.startRotation.y), Math.degrees(this.rotation.y),
             (this.percentComplete * 3) > 1 ? 1 : this.percentComplete * 3);
 
-          value = Math.degrees(value);
+
+
+  console.info(value);
+
           if (value < 0) {
              value = 360 + value;
           }
